fb52f6dec487ab706506955103a8443b5aea8b95
Rudi Starcevic <ooly.me@gmail.com>
2016-04-17 21:19:24 +0800

    Completed: http://survivejs.com/webpack_react/react_and_flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
-import Note from './Note.jsx';
+
+import AltContainer from 'alt-container';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+import Notes from './Notes.jsx';
+
+// container component - stateful pure class
 
 export default class App extends React.Component {
+
   render() {
-    return <Note />;
+    return (
+      <div>
+      <button
+        className="add-note"
+        onClick={this.addNote}>+</button>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{notes: () => NoteStore.getState().notes}}>
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    );
   }
+
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
+
+  editNote(id, task) {
+    // Don't modify if trying to set an empty value
+    if(!task.trim()) {
+      return;
+    }
+
+    NoteActions.update({id, task});
+  }
+
+  deleteNote(id, e) {
+    e.stopPropagation();
+    NoteActions.delete(id);
+  }
+
 }
